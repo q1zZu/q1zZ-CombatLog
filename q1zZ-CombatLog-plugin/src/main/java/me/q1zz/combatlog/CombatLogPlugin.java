b1b9a6589d14ba94a7db1511b380012cf369d5b2
q1zZ <q1zZ@zetcraft.pl>
2024-02-29 23:58:43 +0100

    Start recoding combat plugin

---
+++ q1zZ-CombatLog-plugin/src/main/java/me/q1zz/combatlog/CombatLogPlugin.java
@@ -0,0 +1,17 @@
+package me.q1zz.combatlog;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CombatLogPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
